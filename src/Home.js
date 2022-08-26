85837ee254aa03e87ce013d3952c4c4222caf1ce
Kyle Tillson <kyletillson004@gmail.com>
2022-08-26 15:29:28 -0600

    gif

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
     <br></br>
     <div className='Header'>Welcome to the Ricky and Morty Hub!</div>
     <br></br>
-    <img className='Image' src='https://resizing.flixster.com/RUJfAiDlQTatJprIYxWM3LUAZ9E=/ems.cHJkLWVtcy1hc3NldHMvdHZzZWFzb24vUlRUVjk2MjQ4OC53ZWJw' alt='Rick and Morty'/>
+    <img className='Image' src='https://64.media.tumblr.com/51015ec638a516f7f7d353ca198a5091/tumblr_pdbo9wBAe11xd0gvgo1_1280.gifv' alt='Rick and Morty'/>
     <p className='Paragraph'>In our hub, you can view your favorite Rick and Morty characters and their unique characteristics!</p>
     <br></br>
       <br></br>
